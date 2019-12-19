807dd873d93096689eb5447530ed3b9fa95f1aa2
Никита Узкий <nikitauzkij@Nikitas-MacBook-Air.local>
2019-12-19 10:03:22 +0300

    Временное решение проблемы с формированием массива пиццерий

--- app/getpizzaslist.py
+++ app/getpizzaslist.py
@@ -6,27 +6,20 @@ class GetPizzeriaList():
 
     def __init__(self):
         self.driver = VKLogin().set_connection()
-        self.date_list = []
+        self.main_dict = {}
 
-    def get_pizzerias_list(self):
+    def get_pizzerias_data(self):
         WebDriverWait(self.driver, 100).until(
             lambda is_element_available: self.driver.find_element_by_class_name('pizzeria__list'))
-        pizzerias__list = self.driver.find_elements_by_xpath('//div[@class="pizzeria__list"]/h2')
-        self.get_available_inspection() # Вызов функции для тестирования
+        pizzerias__list = self.driver.find_elements_by_class_name('pizzeria__list')
+        return self.get_available_inspection(pizzerias__list)
 
-        return pizzerias__list
+    def get_available_inspection(self, pizzerias__list):
 
-    def get_available_inspection(self):
-        available_inspection = self.driver.find_elements_by_class_name('type__date')
-
-        for value in available_inspection:
-            if value.text != 'Нет свободных дат':
-                self.date_list.append(value.text)
+        for pizzeria_element in pizzerias__list:
+            self.main_dict.update(pizzeria_element.text)
         
-        if len(self.date_list) == 0:
-            return False
-        
-        return self.date_list        
+        return self.main_dict        
                 
 # Для каждой пицерии свои даты или общий список (парсить блоками и добавлять в рестораны), 
 # либо создавать список [Название ресторана:даты проверок, ...] Посредством проверки каждого pizzeria__list =>
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app.getpizzaslist import GetPizzeriaList
 
 if __name__ == "__main__":
-    GetPizzeriaList().get_pizzerias_list()
+    GetPizzeriaList().get_pizzerias_data()
