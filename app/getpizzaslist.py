0084c451b80e2a3d80a33ae88945a732008fdd6f
Никита Узкий <nikitauzkij@Nikitas-MacBook-Air.local>
2019-12-20 23:45:02 +0300

    Добавил формирование списка пиццерий с проверками

--- app/getpizzaslist.py
+++ app/getpizzaslist.py
@@ -1,3 +1,5 @@
+import re
+
 from selenium.webdriver.support.wait import WebDriverWait
 
 from app.login import VKLogin
@@ -6,27 +8,30 @@ class GetPizzeriaList():
 
     def __init__(self):
         self.driver = VKLogin().set_connection()
-        self.main_dict = {}
+        self.main_list = []
 
-    def get_pizzerias_data(self):
+    def get_pizzeria_data(self):
         WebDriverWait(self.driver, 100).until(
             lambda is_element_available: self.driver.find_element_by_class_name('pizzeria__list'))
-        pizzerias__list = self.driver.find_elements_by_class_name('pizzeria__list')
-        return self.get_available_inspection(pizzerias__list)
-
-    def get_available_inspection(self, pizzerias__list):
-
-        for pizzeria_element in pizzerias__list:
-            self.main_dict.update(pizzeria_element.text)
         
-        return self.main_dict        
-                
-# Для каждой пицерии свои даты или общий список (парсить блоками и добавлять в рестораны), 
-# либо создавать список [Название ресторана:даты проверок, ...] Посредством проверки каждого pizzeria__list =>
-# Для каждой пиццери добавлять даты, даже если их нет если название пиццерии совпадает в названием в методе получения даты, то добавить
-# этому ресторану полученные даты
+        pizzerias_data_list = self.driver.find_elements_by_class_name('pizzeria__list')
+        return self.get_available_inspection(pizzerias_data_list)
 
-# Проверка доступных проверок  
-# TO DO Распарсить даты, придумать интеграцию с сервисами (?) (Vk, Mattermost, WhatsApp)
+    def get_available_inspection(self, pizzerias_data_list):
+        for elem in pizzerias_data_list:
+            # result = re.findall('Нет свободных дат', elem.text) Вариант через регулярные выражения
+            result = (elem.text).count('Нет свободных дат')
+            if result != 3:
+                self.main_list.append(elem.text)
+
+        if len(self.main_list) != 0:
+            return self.main_list
+        
+        return ['Свободных проверок нет']
+
+
+               
+                
+# Придумать интеграцию с сервисами (?) (Vk, Mattermost, WhatsApp)
 # Продумать периодичность запросов к сайту
 # Продумать возможность автоматической записи на проверку
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app.getpizzaslist import GetPizzeriaList
 
 if __name__ == "__main__":
-    GetPizzeriaList().get_pizzerias_data()
+    GetPizzeriaList().get_pizzeria_data()
