48b4bcb7a7954c6eb779cd89a214ad30e0936bd3
Никита Узкий <nikitauzkij@Nikitas-MacBook-Air.local>
2019-12-21 15:18:29 +0300

    Добавил README

--- app/vk_module.py
+++ app/vk_module.py
@@ -1,7 +1,8 @@
+import json
 import vk_api
 from vk_api.utils import get_random_id
 
-TOKEN = '192639fb972139b5b58d6b1bbdc19e25c5290466defb155694ff1d0bf83c339d36ace50e9decdb9f6b577'
+TOKEN = json.loads(open('static/config.json').read())['token']
 
 class SendMessageToVk():
 
