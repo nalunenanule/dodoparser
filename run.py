6f8d86da2616553989083e734005809687aabde2
Никита Узкий <nikita.yzkii@gmail.com>
2020-01-06 13:43:44 +0300

    Убрал Flask

--- run.py
+++ run.py
@@ -1,22 +1,13 @@
-from flask import Flask, request
-
 from app.getpizzaslist import GetPizzeriaList, AvailibleInspections
 from app.vk_module import SendMessageToVk
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
 def actions_with_insperction_list():
-    data = request.get_json()
-    if data['type'] == 'message_new':
-        if data['object']['text'] == 'Доступные проверки':
-            GetPizzeriaList()
-            all_inspections = AvailibleInspections()
-            SendMessageToVk().send_message(all_inspections.inspections, all_inspections.user_vk_id)
-    return 'ok'
-    
+    GetPizzeriaList()
+    all_inspections = AvailibleInspections()
+    SendMessageToVk().send_message(all_inspections.inspections, all_inspections.user_vk_id)
+
 if __name__ == '__main__':
-    app.run()
+    actions_with_insperction_list()
 
 # Делать проверку по запросу и по времени 
 # Сделать интеграцию с Discord (Возможно Docker)
