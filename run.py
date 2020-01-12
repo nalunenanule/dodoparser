b16731e812cd367817ff49fcb872e6848c23d919
Никита Узкий <nikita.yzkii@gmail.com>
2020-01-12 19:35:19 +0300

    Сделал версию для деплоя

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
+from flask import Flask, request
+
 from app.getpizzaslist import GetPizzeriaList, AvailibleInspections
 from app.vk_module import SendMessageToVk
 
+app = Flask(__name__)
+confirmation_code = 'b30bb4f4'
+
+@app.route('/', methods=['POST'])
 def actions_with_insperction_list():
-    GetPizzeriaList()
-    all_inspections = AvailibleInspections()
-    SendMessageToVk().send_message(all_inspections.inspections, all_inspections.user_vk_id)
+    data = request.get_json(force=True, silent=True)
+
+    if not data or 'type' not in data:
+        return 'not ok'
+
+    if data['type'] == 'confirmation':
+        return confirmation_code
+
+    elif data['type'] == 'message_new':
+        if data['text'] == 'Доступные проверки':
+            GetPizzeriaList()
+            all_inspections = AvailibleInspections()
+            SendMessageToVk().send_message(all_inspections.inspections, all_inspections.user_vk_id)
+    return 'ok'
 
 if __name__ == '__main__':
-    actions_with_insperction_list()
+    app.run()
 
 # Делать проверку по запросу и по времени 
 # Сделать интеграцию с Discord (Возможно Docker)
